e203f666063495534cb3fcf68348caa8b9e7d057
Jacob <jacobwamsley12@gmail.com>
2022-01-31 13:44:04 -0700

    P7 -P14 complete

--- person.sql
+++ person.sql
@@ -37,3 +37,43 @@ from person
 where age > 20
 
 --q7
+select * 
+from person
+where age = 18
+
+--Q8
+select * 
+from person
+where age < 20 or age > 30
+
+--Q9
+select * 
+from person
+where age != 27
+
+--Q10
+select * 
+from person
+where favorite_color != 'red'
+
+--11
+select * 
+from person
+where favorite_color != 'red' and 
+favorite_color != 'blue';
+
+--12
+select * 
+from person
+where favorite_color = 'orange' or 
+favorite_color = 'green';
+
+--13
+select * 
+from person
+where favorite_color in ('blue','orange','green')
+
+--14
+select * 
+from person
+where favorite_color in ('purple', 'yellow')
