a8052dad36aff185303a7864da7fbd72bc144810
Jacob <jacobwamsley12@gmail.com>
2022-01-31 20:36:47 -0700

    Intial commit

---
+++ delete.sql
@@ -0,0 +1,18 @@
+--1
+create table animals (
+  id serial primary key,
+  name varchar(15),
+  type varchar(25),
+  age int
+ )
+
+-- INSERT INTO animals ( name, type, age ) 
+-- VALUES ('Leo', 'lion', 12),
+-- ('Jerry', 'mouse', 4),
+-- ('Marty', 'zebra', 10),
+-- ('Gloria', 'hippo', 8),
+-- ('Alex', 'lion', 9),
+-- ('Melman', 'giraffe', 15),
+-- ('Nala', 'lion', 2),
+-- ('Marie', 'cat', 1),
+-- ('Flounder', 'fish', 8);
